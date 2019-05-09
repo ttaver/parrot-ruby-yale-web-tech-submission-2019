18623e12dfe18ec2173bd896331d2af7bd670870
interpreted-refactoring-0526 <taungon@brandeis.edu>
2019-05-09 08:59:50 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
+def parrot( value = "Squawk!")
   puts("Squawk!")
-  "Squak!"
+  "#{value}"
 end
 
 parrot("Pretty Bird!")
